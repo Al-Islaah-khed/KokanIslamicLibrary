da185cd1fcc0feba5a106d8e0c74273adb9b532e
AmmarTheDeveloper <maram28896@gmail.com>
2025-04-30 21:52:07 +0530

    fixed the access token expiry minutes str to int

--- server/app/config.py
+++ server/app/config.py
@@ -4,7 +4,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     JWT_ALGORITHM: str
     SECRET_KEY : str
-    ACCESS_TOKEN_EXPIRE_MINUTES : str
+    ACCESS_TOKEN_EXPIRE_MINUTES : int
 
     class Config:
         env_file = "../.env"
